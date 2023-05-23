f92403dc0d6da078ff0efb51601aae19b60f75ef
Hanna <anna_kon@bigmir.net>
2023-05-23 22:29:05 +0300

    added functions, routes, in progress

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,76 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+require("colors");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
+const successMsg = `Task completed successfully!`.green;
+const errorMsg = `Something went wrong!`.red;
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(errorMsg);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const noContactMsg = `There is no contact with ${contactId} id!`.red;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === contactId);
+    console.log(result);
+    if (result) {
+      console.log(successMsg);
+      return result;
+    }
+    throw new Error();
+  } catch (error) {
+    console.log(noContactMsg);
+    return null;
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const noContactMsg = `There is no contact with ${contactId} id!`.red;
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      console.log(noContactMsg);
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log(successMsg);
+    return result;
+  } catch (error) {
+    console.log(errorMsg);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts();
+    const { name, email, phone } = body;
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log(successMsg);
+    return newContact;
+  } catch (error) {
+    console.log(errorMsg);
+  }
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +78,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,83 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
+const { nanoid } = require("nanoid");
+const contactsOperations = require("../../models/contacts");
+// const contacts = [
+//   {
+//     id: "AeHIrLTr6JkxGE6SN-0Rw",
+//     name: "Allen Raymond",
+//     email: "nulla.ante@vestibul.co.uk",
+//     phone: "(992) 914-3792",
+//   },
+//   {
+//     id: "qdggE76Jtbfd9eWJHrssH",
+//     name: "Chaim Lewis",
+//     email: "dui.in@egetlacus.ca",
+//     phone: "(294) 840-6685",
+//   },
+//   {
+//     id: "drsAJ4SHPYqZeG-83QTVW",
+//     name: "Kennedy Lane",
+//     email: "mattis.Cras@nonenimMauris.net",
+//     phone: "(542) 451-7038",
+//   },
+// ];
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await contactsOperations.listContacts();
+  // res.json({ message: 'template message' })
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: contacts,
+    },
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const result = await contactsOperations.getContactById(contactId);
+  // console.log(result);
+  if (!result) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: `Product with id = ${contactId} not found!`,
+    });
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  const contacts = await contactsOperations.listContacts();
+  const newContact = { ...req.body, id: nanoid() };
+  contacts.push(newContact);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      result: newContact,
+    },
+  });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+  // const contacts = await contactsOperations.listContacts();
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
