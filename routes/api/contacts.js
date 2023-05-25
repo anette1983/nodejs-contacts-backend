26da831ea7eb885edb21c31417e7122ffad1a8c6
Hanna <anna_kon@bigmir.net>
2023-05-25 20:29:35 +0300

    added decorator function, code refactoring

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,16 +3,21 @@ const express = require("express");
 const router = express.Router();
 const { validation } = require("../../middlewares");
 const schemas = require("../../schemas/contact");
+const ctrlWrapper = require("../../helpers");
 
 const { contacts: ctrl } = require("../../controllers");
 
-router.get("/", ctrl.getAllContacts);
-router.get("/:id", ctrl.getById);
+router.get("/", ctrlWrapper(ctrl.getAllContacts));
+router.get("/:id", ctrlWrapper(ctrl.getById));
 
-router.post("/", validation(schemas.addSchema), ctrl.addContact);
+router.post("/", validation(schemas.addSchema), ctrlWrapper(ctrl.addContact));
 
-router.delete("/:id", ctrl.deleteContact);
+router.delete("/:id", ctrlWrapper(ctrl.deleteContact));
 
-router.put("/:id", validation(schemas.addSchema), ctrl.updateContact);
+router.put(
+  "/:id",
+  validation(schemas.addSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
 
 module.exports = router;
