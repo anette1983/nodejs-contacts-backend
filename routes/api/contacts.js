4df5e15cfc3e800ffaf009936d37098b1397cd7a
Hanna <anna_kon@bigmir.net>
2023-06-01 19:32:13 +0300

    deleted unnecessary code

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,28 +3,27 @@ const express = require("express");
 const router = express.Router();
 const { validation, isValidId } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
-const { ctrlWrapper } = require("../../helpers");
 
 const { contacts: ctrl } = require("../../controllers");
 
-router.get("/", ctrlWrapper(ctrl.getAllContacts));
-router.get("/:id", isValidId, ctrlWrapper(ctrl.getById));
+router.get("/", ctrl.getAllContacts);
+router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", validation(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:id", isValidId, ctrlWrapper(ctrl.deleteContact));
+router.delete("/:id", isValidId, ctrl.deleteContact);
 
 router.put(
   "/:id",
   isValidId,
   validation(schemas.addSchema),
-  ctrlWrapper(ctrl.updateContact)
+  ctrl.updateContact
 );
 router.patch(
   "/:id/favorite",
   isValidId,
   validation(schemas.updateFavoriteSchema),
-  ctrlWrapper(ctrl.updateFavorite)
+  ctrl.updateFavorite
 );
 
 module.exports = router;
